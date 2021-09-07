9505349e222fa82c3d0678ec1f6250d12cb5a341
Eldan-coding <81482780+Eldan-coding@users.noreply.github.com>
2021-09-07 08:10:45 -0500

    API RESTful

--- APIRESTful.js
+++ APIRESTful.js
@@ -1,9 +1,17 @@
-import express, { json }  from "express";
+import express  from "express";
 
 const app= express();
 const PORT= 8080;
+const router = express.Router();
 
-const productos=[];
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use('/api', router);
+
+app.use(express.static('public'));
+
+let productos=[];
 let id=0;
 
 class Producto{
@@ -40,14 +48,11 @@ const server = app.listen (PORT, ()=>{
 });
 server.on('error', error=>console.log('Error en servidor',error));
 
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-
-app.get('/',(req,res)=>{
+router.get('/', (req,res)=>{
     res.send("<h1>Inicio Del Programa</h1>");
 });
 
-app.get('/api/productos/listar',(req,res)=>{
+router.get('/productos/listar',(req,res)=>{
     if(productos.length > 0){
         res.json(productos);
     }else{
@@ -55,7 +60,7 @@ app.get('/api/productos/listar',(req,res)=>{
     }
 });
 
-app.get('/api/productos/listar/:id',(req,res)=>{
+router.get('/productos/listar/:id',(req,res)=>{
     let params = req.params;
     let resultado={error: 'producto no encontrado'};
     for (let index = 0; index < productos.length; index++) {
@@ -67,11 +72,46 @@ app.get('/api/productos/listar/:id',(req,res)=>{
     res.json(resultado)
 });
 
-app.post('/api/productos/guardar',(req,res)=>{
+router.post('/productos/guardar',(req,res)=>{
     let body = req.body;
+    console.log(body)
     const datos=Object.values(body);
 
     let product=new Producto(datos[0],datos[1],datos[2],getID());
     addProduct(product.getObject());
     res.json(product.getObject())
+});
+
+router.put('/productos/actualizar/:id',(req,res)=>{
+    let params = req.params;
+    let body = req.body;
+    const datos=Object.values(body);
+
+    let resultado={error: 'producto no actualizado: no se encontro'};
+    for (let index = 0; index < productos.length; index++) {
+        if(productos[index].id==params.id){
+            let product=new Producto(datos[0],datos[1],datos[2],params.id);
+            productos[index]=product.getObject();
+            resultado=product.getObject();
+        }
+    }
+
+    res.json(resultado)
+});
+
+router.delete('/productos/borrar/:id',(req,res)=>{
+    let params = req.params;
+
+    let arrayAux=[];
+    let resultado={error: 'producto no eliminado: no se encontro'};
+    for (let index = 0; index < productos.length; index++) {
+        if(productos[index].id==params.id){
+            resultado=productos[index];
+        }else{
+            arrayAux.push(productos[index]);
+        }
+    }
+    productos=arrayAux;
+
+    res.json(resultado)
 });
